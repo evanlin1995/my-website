65d8deb554f614cb6dba50e66135990324fd5509
evanlin1995 <evanlin1995@gmail.com>
2015-06-25 16:22:58 -0700

    angular tabs
    tab controller works

--- angular-seed/app/app.js
+++ angular-seed/app/app.js
@@ -1,12 +1,34 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+(function() { 
+	// Declare app level module which depends on views, and components
+	// var app = angular.module('myApp', [
+	//   'ngRoute',
+	//   'myApp.view1',
+	//   'myApp.view2',
+	//   'myApp.version'
+	// ]);
+
+	var app = angular.module('myApp', [
+	  'ngRoute',
+	  'myApp.view1',
+	  'myApp.view2',
+	  'myApp.version'
+	]);
+
+	app.controller('TabController', function($scope) {
+		// $scope.visible = false;
+		this.tab = 1;
+		this.setTab = function(newValue) {
+			this.tab = newValue;
+		};
+		this.isSet = function(tabValue) {
+			return this.tab === tabValue;
+		}
+	});
+
+	app.config(['$routeProvider', function($routeProvider) {
+	  $routeProvider.otherwise({redirectTo: '/view1'});
+	}]);
+
+})();
--- angular-seed/app/assets/js/initialize-slider.js
+++
@@ -1,4 +0,0 @@
-jQuery(document).ready(function ($) {
-	var options = { $AutoPlay: true };
-	var jssor_slider1 = new $JssorSlider$('slider1_container', options);
-});
